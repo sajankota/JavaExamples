2483b104fc60a1ad00a1505a9fb32e485b1c2a0e
Sajan Kota <sajankota001@gmail.com>
2019-03-12 18:57:49 +0530

    added new class

---
+++ javadatatypes/src/com/roundcodebox/javadatatypes/JavaDataTypes.java
@@ -0,0 +1,18 @@
+package com.roundcodebox.javadatatypes;
+
+public class JavaDataTypes {
+
+	public static void main(String[] args) {
+		int maxInt;
+		short myShort;
+		long myLong;
+		byte myByte;
+		char myChar;
+		float myFloat;
+		boolean myBoolean;
+		double myDouble;
+		
+
+	}
+
+}
